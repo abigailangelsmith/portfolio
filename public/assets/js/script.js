cf850b2287a1cea6bff5d283080123e3924abf30
Abigail Smith <abigailangelsmith@outlook.com>
2022-04-27 12:43:49 +0100

    PP-19: Adding click event to actually update the css colour scheme and change the icon scheme

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,8 @@
 (
     () => {
+
+        // NAVIGATION
+
         // Get Elements
         let navigationElements = document.querySelectorAll("nav a p");
 
@@ -15,5 +18,36 @@
         navigationElements.forEach((element) => {
             element.addEventListener('click', onClickNavigationActiveToggle);
         });
+
+        // COLOUR SCHEME
+
+        let root = document.documentElement;
+        let body = document.body;
+        let colourSchemeIcons = document.querySelectorAll("#colour-changing-options > svg");
+
+        function onClickColourSchemeChange(event) {
+            // get target dataset info
+            const scheme = event.target.dataset.scheme;
+
+            // update css root properties with the chosen variable
+            root.style.setProperty('--main-colour', 'var(--' + scheme + '_main-colour');
+            root.style.setProperty('--accent-colour', 'var(--' + scheme + '_accent-colour');
+            root.style.setProperty('--opacity-colour', 'var(--' + scheme + '_opacity-colour');
+
+            // change the icon and dataset property - use css to fill colours using the dataset property
+            const oldScheme = body.dataset.currentColourScheme;
+
+            event.target.dataset.scheme = oldScheme;
+            body.dataset.currentColourScheme = scheme;
+
+            // change css image path for nav active arrows
+
+            // change picture to have the correct border colour - as not css controlled, can use css display property to change
+        }
+
+        colourSchemeIcons.forEach((icon) => {
+            icon.addEventListener('click', onClickColourSchemeChange);
+        });
+
     }
 )();
