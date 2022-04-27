a1027261f220528a0e63c80bddf2ac28edd365fb
Abigail Smith <21154948+abigailangelsmith@users.noreply.github.com>
2022-04-27 21:15:53 +0100

    Feature/PP-19 Implementing Colour Scheme Options (#33)
    * PP-19: Updating to use CSS variables for colour scheme
    
    * PP-19: Replacing Sass colour variables with CSS colour variables
    
    * PP-19: Adding click event to actually update the css colour scheme and change the icon scheme
    
    * PP-19: Making icon colours dynamic
    
    * PP-19: Updating navigation arrow svgs to match colour scheme
    
    * PP-19: Make svg fill colours dynamic
    
    * PP-19: Removing unneeded comments

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,8 @@
 (
     () => {
+
+        // NAVIGATION
+
         // Get Elements
         let navigationElements = document.querySelectorAll("nav a p");
 
@@ -15,5 +18,31 @@
         navigationElements.forEach((element) => {
             element.addEventListener('click', onClickNavigationActiveToggle);
         });
+
+        // COLOUR SCHEME
+
+        let root = document.documentElement;
+        let body = document.body;
+        let colourSchemeIcons = document.querySelectorAll("#colour-changing-options > svg");
+
+        function onClickColourSchemeChange(event) {
+            const scheme = event.target.dataset.scheme;
+
+            root.style.setProperty('--main-colour', 'var(--' + scheme + '_main-colour)');
+            root.style.setProperty('--accent-colour', 'var(--' + scheme + '_accent-colour)');
+            root.style.setProperty('--opacity-colour', 'var(--' + scheme + '_opacity-colour)');
+            root.style.setProperty('--main-nav-arrow', 'var(--' + scheme + '_main-nav-arrow)');
+            root.style.setProperty('--accent-nav-arrow', 'var(--' + scheme + '_accent-nav-arrow)');
+        
+            const oldScheme = body.dataset.currentColourScheme;
+
+            event.target.dataset.scheme = oldScheme;
+            body.dataset.currentColourScheme = scheme;
+        }
+
+        colourSchemeIcons.forEach((icon) => {
+            icon.addEventListener('click', onClickColourSchemeChange);
+        });
+
     }
 )();
