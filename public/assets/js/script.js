6beee1b042c439a0e3fd0754d2eed1456e42698e
Abigail Smith <abigailangelsmith@outlook.com>
2022-04-27 21:13:55 +0100

    PP-19: Removing unneeded comments

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -26,26 +26,18 @@
         let colourSchemeIcons = document.querySelectorAll("#colour-changing-options > svg");
 
         function onClickColourSchemeChange(event) {
-            // get target dataset info
             const scheme = event.target.dataset.scheme;
 
-            // update css root properties with the chosen variable
             root.style.setProperty('--main-colour', 'var(--' + scheme + '_main-colour)');
             root.style.setProperty('--accent-colour', 'var(--' + scheme + '_accent-colour)');
             root.style.setProperty('--opacity-colour', 'var(--' + scheme + '_opacity-colour)');
             root.style.setProperty('--main-nav-arrow', 'var(--' + scheme + '_main-nav-arrow)');
             root.style.setProperty('--accent-nav-arrow', 'var(--' + scheme + '_accent-nav-arrow)');
         
-
-            // change the icon and dataset property - use css to fill colours using the dataset property
             const oldScheme = body.dataset.currentColourScheme;
 
             event.target.dataset.scheme = oldScheme;
             body.dataset.currentColourScheme = scheme;
-
-            // change css image path for nav active arrows
-
-            // change picture to have the correct border colour - as not css controlled, can use css display property to change
         }
 
         colourSchemeIcons.forEach((icon) => {
